29384ea77cfe892bcb18eac60a7f54387b980f76
Alan <alanlucas132015@gmail.com>
2019-05-28 11:36:03 -0300

    Pagina

---
+++ src/js/javas.js
@@ -0,0 +1,10 @@
+
+body{
+animation: multifondo 25s infinite
+}
+@-webkit-keyframes multifondo {
+    0%   {background-color: red;}
+    25%  {background-color: #dddccc;}
+    75%  {background-color: green;}
+    100% {background-color: #abcabc;;}
+}
